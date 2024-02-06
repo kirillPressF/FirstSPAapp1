5bd257aff16df3952ca0fb0e046d4b7e328085c5
Prokhorenko Kirill <prohorenko25@outlook.com>
2024-02-07 01:30:10 +0200

    Revert "Revert "PostService""
    This reverts commit 9300aac338a20129ec0b5721983ff870ec0efa9b.

---
+++ FirstSPAapp.Server/Controllers/PostsController.cs
@@ -0,0 +1,37 @@
+﻿using FirstSPAapp.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstSPAapp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpPost]
+        public object Create(PostModel model)
+        {
+
+        }
+        [HttpPatch]
+        public object Update(PostModel model)
+        {
+
+        }
+        [HttpGet("{id}")]
+        public object Get(int id)
+        {
+
+        }
+        [HttpGet]
+        public object GetAll()
+        {
+
+        }
+        [HttpDelete("{id}")]
+        public object Delete(int id)
+        {
+
+        }
+    }
+}
