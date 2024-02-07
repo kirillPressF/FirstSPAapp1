d44160149c98873aba3ee1dfaae26f29ab948b12
Prokhorenko Kirill <prohorenko25@outlook.com>
2024-02-07 02:41:56 +0200

    PostServiceCRUD

---
+++ FirstSPAapp.Server/Data/MyDataContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstSPAapp.Server.Models;
+
+namespace FirstSPAapp.Server.Data
+{
+    public class MyDataContext
+    {
+        public List<PostModel> Posts { get; set; }
+
+        public MyDataContext()
+        {
+            Posts = new List<PostModel>();
+        }
+    }
+}
--- FirstSPAapp.Server/Services/PostService.cs
+++ FirstSPAapp.Server/Services/PostService.cs
@@ -1,6 +1,55 @@
-﻿namespace FirstSPAapp.Server.Services
+﻿using FirstSPAapp.Server.Data;
+using FirstSPAapp.Server.Models;
+using FirstSPAapp.Server.Services.Interfaces;
+
+namespace FirstSPAapp.Server.Services
 {
-    public class PostService
+    public class PostService : IPostService
     {
+        private MyDataContext _dataContext; 
+        public PostService(MyDataContext context)
+        {
+            this._dataContext = context;
+        }
+        public PostModel Create(PostModel model)
+        {
+            var last = _dataContext.Posts.LastOrDefault();
+            int newId = last == null ? 1 : last.Id + 1;
+            model.Id = newId;
+            _dataContext.Posts.Add(model);
+            return model;
+        }
+
+        public PostModel Update(PostModel model)
+        {
+            var modelToUpdate = _dataContext.Posts.FirstOrDefault(x=>x.Id==model.Id);
+            if (modelToUpdate != null)
+            {
+                modelToUpdate.Header = model.Header;
+                modelToUpdate.Text = model.Text;
+            }
+
+            return modelToUpdate;
+        }
+        public void Delete(int id)
+        {
+            if (id >= 0 && _dataContext.Posts.Any(x => x.Id == id))
+            {
+                _dataContext.Posts.Remove(_dataContext.Posts.FirstOrDefault(x => x.Id == id));
+            }
+        }
+
+        public PostModel Get(int id)
+        {
+            if (id >= 0 && _dataContext.Posts.Any(x => x.Id == id))
+            {
+                var modelToReturn = _dataContext.Posts.FirstOrDefault(x => x.Id == id);
+            }
+
+            return null;
+        }
+
+        public List<PostModel> Get() =>_dataContext.Posts;
+
     }
 }
